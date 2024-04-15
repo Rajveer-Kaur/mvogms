e10c96d184b296c086a72adeac957c6f8d6bcbe4
Rajveer-Kaur <rajveerkaur20952@gmail.com>
2024-04-15 12:28:08 +0530

    First Commit

---
+++ about.php
@@ -0,0 +1,12 @@
+
+<div class="content py-3">
+    <div class="card rounded-0 card-outline card-warning shadow" >
+        <div class="card-body rounded-0">
+            <h2 class="text-center">About</h2>
+            <center><hr class="bg-navy border-warning w-25 border-2"></center>
+            <div>
+                <?= file_get_contents("about.html") ?>
+            </div>
+        </div>
+    </div>
+</div>
